0dbe5aed369c75a37204d757c8bb7c7c9e1c1a15
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-08-10 07:01:44 -0300

    criação de algumas tabelas no db

--- lib/app/config/db.dart
+++ lib/app/config/db.dart
@@ -11,12 +11,14 @@ class DB {
             "CREATE TABLE products (id INTEGER PRIMARY KEY, name TEXT NOT NULL, price REAL, quantity INTEGER, cost_value REAL, profit_value REAL NOT NULL)");
 
         db.execute(
-            "CREATE TABLE sales (id INTEGER PRIMARY KEY, date_sale TEXT NOT NULL, value_total REAL NOT NULL, profit_value_total REAL)"); // sales - Vendas
+            "CREATE TABLE sales (id INTEGER PRIMARY KEY, date_sale TEXT NOT NULL, value_total REAL NOT NULL, profit_value_total REAL, name_client TEXT NULL)"); // sales - Vendas
 
         db.execute(
-            "CREATE TABLE items_sales (id INTEGER PRIMARY KEY, price_product REAL NOT NULL, product_id INTEGER NOT NULL, sale_id INTEGER NOT NULL, FOREIGN KEY (product_id) REFERENCES products(id), ON DELETE SET NULL, FOREIGN KEY (sale_id) REFERENCES sales(id) ON DELETE CASCADE)");
+            "CREATE TABLE items_sales (id INTEGER PRIMARY KEY, quantity_items INTEGER, sub_total REAL NOT NULL, price_product REAL NOT NULL, product_id INTEGER NOT NULL, sale_id INTEGER NOT NULL, FOREIGN KEY (product_id) REFERENCES products(id), ON DELETE SET NULL, FOREIGN KEY (sale_id) REFERENCES sales(id) ON DELETE CASCADE)");
 
-        
+        db.execute("CREATE TABLE services (id INTEGER PRIMARY KEY, description TEXT NOT NULL, price REAL NOT NULL)");
+
+        db.execute("CREATE TABLE provision_of_services (id INTEGER PRIMARY KEY, date TEXT NOT NULL, time TEXT NOT NULL)");
       },
       version: 1,
     );
