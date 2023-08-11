ad73dd452ba5a82774f17959d46e3c47931acd19
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-08-11 06:32:44 -0300

    incrementando mais colunas

--- lib/app/config/db.dart
+++ lib/app/config/db.dart
@@ -8,19 +8,24 @@ class DB {
       path.join(dbPath, "kaikebarbearia.db"),
       onCreate: (db, version) {
         db.execute(
-            "CREATE TABLE products (id INTEGER PRIMARY KEY, name TEXT NOT NULL, price REAL, quantity INTEGER, cost_value REAL, profit_value REAL NOT NULL)");
+          "CREATE TABLE products (id INTEGER PRIMARY KEY, name TEXT NOT NULL, price REAL, quantity INTEGER, cost_value REAL, profit_value REAL NOT NULL)",
+        );
 
         db.execute(
-            "CREATE TABLE sales (id INTEGER PRIMARY KEY, date_sale TEXT NOT NULL, value_total REAL NOT NULL, profit_value_total REAL, name_client TEXT NULL)"); // sales - Vendas
+          "CREATE TABLE sales (id INTEGER PRIMARY KEY, date_sale TEXT NOT NULL, value_total REAL NOT NULL, profit_value_total REAL, name_client TEXT NULL)",
+        ); // sales - Vendas
 
         db.execute(
-            "CREATE TABLE items_sales (id INTEGER PRIMARY KEY, quantity_items INTEGER, sub_total REAL NOT NULL, price_product REAL NOT NULL, product_id INTEGER NOT NULL, sale_id INTEGER NOT NULL, FOREIGN KEY (product_id) REFERENCES products(id), ON DELETE SET NULL, FOREIGN KEY (sale_id) REFERENCES sales(id) ON DELETE CASCADE)");
+          "CREATE TABLE items_sales (id INTEGER PRIMARY KEY, quantity_items INTEGER, sub_total REAL NOT NULL, price_product REAL NOT NULL, product_id INTEGER NOT NULL, sale_id INTEGER NOT NULL, FOREIGN KEY (product_id) REFERENCES products(id), ON DELETE SET NULL, FOREIGN KEY (sale_id) REFERENCES sales(id) ON DELETE CASCADE)",
+        );
 
         db.execute(
-            "CREATE TABLE services (id INTEGER PRIMARY KEY, description TEXT NOT NULL, price REAL NOT NULL)");
+          "CREATE TABLE services (id INTEGER PRIMARY KEY, description TEXT NOT NULL, price REAL NOT NULL)",
+        );
 
         db.execute(
-            "CREATE TABLE provision_of_services (id INTEGER PRIMARY KEY, date TEXT NOT NULL, time TEXT NOT NULL)");
+          "CREATE TABLE provision_of_services (id INTEGER PRIMARY KEY, date TEXT NOT NULL, time TEXT NOT NULL, service_id INTEGER, FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE)",
+        );
 
         db.execute(
           "CREATE TABLE spending (id INTEGER PRIMARY KEY, name TEXT NOT NULL, price REAL NOT NULL, quantity INTEGER NOT NULL)", // spending = gastos da barbearia
