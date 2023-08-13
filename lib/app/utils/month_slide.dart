619ac53c1af4466b8bcc55b19b7d15c44c3503e8
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-08-13 20:07:40 -0300

    month

---
+++ lib/app/utils/month_slide.dart
@@ -0,0 +1,13 @@
+final List<Map<String, dynamic>> date = [
+  {"month": "Janeiro", "number": 1},
+  {"month": "Fevereiro", "number": 2},
+  {"month": "Março", "number": 3},
+  {"month": "Maio", "number": 4},
+  {"month": "Junho", "number": 6},
+  {"month": "Julho", "number": 7},
+  {"month": "Agosto", "number": 8},
+  {"month": "Setembro", "number": 9},
+  {"month": "Outubro", "number": 10},
+  {"month": "Novembro", "number": 11},
+  {"month": "Dezembro", "number": 12},
+];
