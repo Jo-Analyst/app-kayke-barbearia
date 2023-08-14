dfdbe88a87dfee46f0199ff6948c9c360213a7f4
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-08-14 20:30:07 -0300

    implementando meio de pagamento no balanço do mês

--- lib/app/template/finance_sales.dart
+++ lib/app/template/finance_sales.dart
@@ -20,6 +20,53 @@ class _FinanceSalesState extends State<FinanceSales> {
     {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
   ]; // produtos vendidos
 
+  final List<Map<String, dynamic>> payments = [
+    {
+      "icon": const Icon(
+        Icons.pix,
+        color: Colors.green,
+      ),
+      "name": "Pix",
+      "quantity": 2,
+      "value": 100,
+    },
+    {
+      "icon": const Icon(
+        Icons.credit_card,
+        color: Colors.purple,
+      ),
+      "name": "Cartão de crédito",
+      "quantity": 3,
+      "value": 150
+    },
+    {
+      "icon": const Icon(
+        Icons.credit_card,
+        color: Colors.purple,
+      ),
+      "name": "Cartão de débito",
+      "quantity": 4,
+      "value": 200
+    },
+    {
+      "icon": const Icon(
+        Icons.attach_money,
+        color: Colors.green,
+      ),
+      "name": "Dinheiro",
+      "quantity": 5,
+      "value": 250
+    },
+    {
+      "icon": const Icon(
+        Icons.money_off,
+        color: Colors.red,
+      ),
+      "name": "Fiado",
+      "quantity": 2,
+      "value": 100
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -42,9 +89,22 @@ class _FinanceSalesState extends State<FinanceSales> {
             ),
           ),
           const SizedBox(height: 10),
+          Divider(color: Theme.of(context).primaryColor),
+          Container(
+            margin: const EdgeInsets.only(left: 10),
+            alignment: Alignment.topLeft,
+            child: const Text(
+              "Vendas:",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Divider(color: Theme.of(context).primaryColor),
           Container(
             color: const Color.fromARGB(17, 63, 81, 181),
-            margin: const EdgeInsets.symmetric(horizontal: 10),
+            margin: const EdgeInsets.all(10),
             height: 200,
             child: productsSold.isEmpty
                 ? const Center(
@@ -101,8 +161,9 @@ class _FinanceSalesState extends State<FinanceSales> {
                                             numberFormat
                                                 .format(product["price"]),
                                             style: const TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w500),
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w500,
+                                            ),
                                           ),
                                         ),
                                       ),
@@ -117,6 +178,70 @@ class _FinanceSalesState extends State<FinanceSales> {
                       ).toList(),
                     ),
                   ),
+          ),
+          Divider(color: Theme.of(context).primaryColor),
+          Container(
+            margin: const EdgeInsets.only(left: 10),
+            alignment: Alignment.topLeft,
+            child: const Text(
+              "Pagamentos:",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Divider(color: Theme.of(context).primaryColor),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Column(
+              children: payments.map(
+                (payment) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: Row(
+                            children: [
+                              payment["icon"] ?? const Text(""),
+                              const SizedBox(width: 5),
+                              Text(
+                                payment["name"],
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: MediaQuery.of(context).size.width / 8,
+                          child: Text(
+                            payment["quantity"].toString(),
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            width: MediaQuery.of(context).size.width / 3,
+                            child: Text(
+                              numberFormat.format(payment["value"]),
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
           ),
         ],
       ),
