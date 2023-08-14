22b560ede2b743e17060ede3526b73b7f4aee1ce
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-08-14 10:50:08 -0300

    Melhoria do design da tela finances_sales

--- lib/app/template/finance_sales.dart
+++ lib/app/template/finance_sales.dart
@@ -10,46 +10,8 @@ class FinanceSales extends StatefulWidget {
 }
 
 class _FinanceSalesState extends State<FinanceSales> {
+  // PrimaryScrollController _controller = PrimaryScrollController();
   final List<Map<String, dynamic>> productsSold = [
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
-    {"name": "Gel Azul", "quantity": 5, "price": 50.00},
-    {"name": "Gel Preto", "quantity": 15, "price": 150.00},
-    {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
     {"name": "Gel Azul", "quantity": 5, "price": 50.00},
     {"name": "Gel Preto", "quantity": 15, "price": 150.00},
     {"name": "maquina de barbear", "quantity": 10, "price": 900.00},
@@ -66,7 +28,7 @@ class _FinanceSalesState extends State<FinanceSales> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
-            numberFormat.format(3000),
+            numberFormat.format(1100),
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.bold,
@@ -80,50 +42,82 @@ class _FinanceSalesState extends State<FinanceSales> {
             ),
           ),
           const SizedBox(height: 10),
-          SizedBox(
+          Container(
+            color: const Color.fromARGB(17, 63, 81, 181),
+            margin: const EdgeInsets.symmetric(horizontal: 10),
             height: 200,
-            child: ListView(
-              shrinkWrap: true,
-              children: productsSold.map((product) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                  child: Row(
-                    children: [
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        width: MediaQuery.of(context).size.width / 2,
-                        child: Text(
-                          product["name"],
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      Container(
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width / 8,
-                        child: Text(
-                          product["quantity"].toString(),
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          alignment: Alignment.centerRight,
-                          width: MediaQuery.of(context).size.width / 3,
-                          child: Text(
-                            numberFormat.format(product["price"]),
-                            style: const TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w500),
-                          ),
-                        ),
-                      ),
-                    ],
+            child: productsSold.isEmpty
+                ? const Center(
+                    child: Text(
+                      "Não há vendas adicionado neste mês.",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  )
+                : Scrollbar(
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: productsSold.map(
+                        (product) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8,
+                              horizontal: 15,
+                            ),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 5),
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        alignment: Alignment.centerLeft,
+                                        width:
+                                            MediaQuery.of(context).size.width /
+                                                2,
+                                        child: Text(
+                                          product["name"],
+                                          style: const TextStyle(fontSize: 20),
+                                        ),
+                                      ),
+                                      Container(
+                                        alignment: Alignment.center,
+                                        width:
+                                            MediaQuery.of(context).size.width /
+                                                8,
+                                        child: Text(
+                                          product["quantity"].toString(),
+                                          style: const TextStyle(fontSize: 20),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          alignment: Alignment.centerRight,
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width /
+                                              3,
+                                          child: Text(
+                                            numberFormat
+                                                .format(product["price"]),
+                                            style: const TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const Divider(),
+                              ],
+                            ),
+                          );
+                        },
+                      ).toList(),
+                    ),
                   ),
-                );
-              }).toList(),
-            ),
           ),
-          
         ],
       ),
     );
