b24f2d7117ed74d83968896b98ede29f70789505
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-08-17 06:53:54 -0300

    Começo do design do fluxo de caixa

--- lib/app/pages/cash_flow_page.dart
+++ lib/app/pages/cash_flow_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_kaike_barbearia/app/utils/convert_values.dart';
 import 'package:flutter/material.dart';
 
 class CashFlowPage extends StatefulWidget {
@@ -8,8 +9,115 @@ class CashFlowPage extends StatefulWidget {
 }
 
 class _CashFlowPageState extends State<CashFlowPage> {
+  DateTime dateSelected = DateTime.now();
+
+  showCalendarPicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2014),
+      lastDate: DateTime.now(),
+    ).then(
+      (date) => setState(() {
+        if (date != null) {
+          dateSelected = date;
+        }
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Meu fluxo de caixa"),);
+    return ListView(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.indigo.withOpacity(.1),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                dateFormat2.format(dateSelected),
+                style: const TextStyle(fontSize: 20),
+              ),
+              IconButton(
+                onPressed: () => showCalendarPicker(),
+                icon: Icon(
+                  Icons.calendar_month_outlined,
+                  size: 35,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              const Text(
+                "Saldo",
+                style: TextStyle(fontSize: 25),
+              ),
+              Text(
+                numberFormat.format(300),
+                style: const TextStyle(
+                  fontSize: 26,
+                  color: Colors.green,
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          color: Colors.indigo.withOpacity(.1),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    numberFormat.format(50),
+                    style: const TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.green,
+                    ),
+                  ),
+                  const Text(
+                    "Total Venda",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    numberFormat.format(250),
+                    style: const TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.green,
+                    ),
+                  ),
+                  const Text(
+                    "Total Serviço",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
--- lib/app/utils/convert_values.dart
+++ lib/app/utils/convert_values.dart
@@ -1,3 +1,6 @@
 import 'package:intl/intl.dart';
 
- final NumberFormat numberFormat = NumberFormat.currency(locale: "pt_BR", symbol: "R\$");
+final NumberFormat numberFormat =
+    NumberFormat.currency(locale: "pt_BR", symbol: "R\$");
+final DateFormat dateFormat1 = DateFormat("dd/MM/yyyy", "pt_BR");
+final DateFormat dateFormat2 = DateFormat("dd 'de' MMMM 'de' yyyy", "pt_BR");
