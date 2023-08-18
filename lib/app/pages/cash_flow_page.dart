a47f504d66257f0f10592ecd602a22096762e39e
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-08-18 06:21:46 -0300

    implementado a lista no fluxo de caixa

--- lib/app/pages/cash_flow_page.dart
+++ lib/app/pages/cash_flow_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_kaike_barbearia/app/template/finance_sale_list.dart';
+import 'package:app_kaike_barbearia/app/template/finance_service.list.dart';
 import 'package:app_kaike_barbearia/app/utils/convert_values.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +13,8 @@ class CashFlowPage extends StatefulWidget {
 class _CashFlowPageState extends State<CashFlowPage> {
   DateTime dateSelected = DateTime.now();
   List<bool> containerButton = [false, false];
+  bool activeContainerSale = false;
+  bool activeContainerService = false;
 
   showCalendarPicker() {
     showDatePicker(
@@ -27,147 +31,181 @@ class _CashFlowPageState extends State<CashFlowPage> {
     );
   }
 
-  changeContainerButton(int index) {
+  changeContainerSale() {
     setState(() {
-      for (int i = 0; i < containerButton.length; i++) {
-        containerButton[i] = false;
-      }
-      containerButton[index] = true;
+      activeContainerSale = !activeContainerSale;
+      if (activeContainerSale) activeContainerService = false;
+    });
+  }
+
+  changeContainerService() {
+    setState(() {
+      activeContainerService = !activeContainerService;
+      if (activeContainerService) activeContainerSale = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          color: Colors.indigo.withOpacity(.1),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                dateFormat2.format(dateSelected),
-                style: const TextStyle(fontSize: 20),
-              ),
-              IconButton(
-                onPressed: () => showCalendarPicker(),
-                icon: Icon(
-                  Icons.calendar_month_outlined,
-                  size: 35,
-                  color: Theme.of(context).primaryColor,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            color: Colors.indigo.withOpacity(.1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  dateFormat2.format(dateSelected),
+                  style: const TextStyle(fontSize: 20),
                 ),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Column(
-            children: [
-              const Text(
-                "Saldo",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                numberFormat.format(300),
-                style: const TextStyle(
-                  fontSize: 26,
-                  color: Colors.green,
-                ),
-              )
-            ],
-          ),
-        ),
-        Container(
-          color: Colors.indigo.withOpacity(.1),
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              InkWell(
-                onTap: () => changeContainerButton(0),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blueGrey,
-                      width: 1,
-                    ),
-                    color: containerButton[0]
-                        ? Theme.of(context).primaryColor
-                        : null,
-                    borderRadius: BorderRadius.circular(10.0),
+                IconButton(
+                  onPressed: () => showCalendarPicker(),
+                  icon: Icon(
+                    Icons.calendar_month_outlined,
+                    size: 35,
+                    color: Theme.of(context).primaryColor,
                   ),
-                  child: Column(
-                    children: [
-                      Text(
-                        numberFormat.format(50),
-                        style: TextStyle(
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Column(
+              children: [
+                const Text(
+                  "Saldo",
+                  style: TextStyle(fontSize: 25),
+                ),
+                Text(
+                  numberFormat.format(300),
+                  style: const TextStyle(
+                    fontSize: 26,
+                    color: Colors.green,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Divider(
+            height: 15,
+            color: Theme.of(context).primaryColor,
+          ),
+          Container(
+            // color: Colors.indigo.withOpacity(.1),
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () => changeContainerSale(),
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.blueGrey,
+                        width: 1,
+                      ),
+                      color: activeContainerSale
+                          ? Theme.of(context).primaryColor
+                          : null,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          numberFormat.format(50),
+                          style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.w400,
+                              color: activeContainerSale
+                                  ? Colors.white
+                                  : Colors.green),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          "Total Venda",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            color: activeContainerSale
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () => changeContainerService(),
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.blueGrey,
+                        width: 1,
+                      ),
+                      color: activeContainerService
+                          ? Theme.of(context).primaryColor
+                          : null,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          numberFormat.format(250),
+                          style: TextStyle(
                             fontSize: 23,
                             fontWeight: FontWeight.w400,
-                            color: containerButton[0]
+                            color: activeContainerService
                                 ? Colors.white
-                                : Colors.green),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        "Total Venda",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                          color:
-                              containerButton[0] ? Colors.white : Colors.black,
+                                : Colors.green,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () => changeContainerButton(1),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blueGrey,
-                      width: 1,
+                        const SizedBox(height: 5),
+                        Text(
+                          "Total Serviço",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            color: activeContainerService
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ],
                     ),
-                    color: containerButton[1]
-                        ? Theme.of(context).primaryColor
-                        : null,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    children: [
-                      Text(
-                        numberFormat.format(250),
-                        style: TextStyle(
-                          fontSize: 23,
-                          fontWeight: FontWeight.w400,
-                          color:
-                              containerButton[1] ? Colors.white : Colors.green,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        "Total Serviço",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                          color:
-                              containerButton[1] ? Colors.white : Colors.black,
-                        ),
-                      ),
-                    ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+          Divider(
+            height: 15,
+            color: Theme.of(context).primaryColor,
+          ),
+          Visibility(
+            visible: activeContainerSale,
+            child: Container(
+              color: Colors.indigo.withOpacity(.1),
+              height: 200,
+              child: const FinanceSaleList(),
+            ),
+          ),
+          Visibility(
+            visible: activeContainerService,
+            child: Container(
+              color: Colors.indigo.withOpacity(.1),
+              height: 200,
+              child: const FinanceServiceList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
