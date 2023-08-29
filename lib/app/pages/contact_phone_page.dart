7f6a12eabd6e93d0b3caa7d9f5f60ff0e030bcca
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-08-29 19:50:34 -0300

    implementado busca na lista de contatos

--- lib/app/pages/contact_phone_page.dart
+++ lib/app/pages/contact_phone_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_kaike_barbearia/app/utils/search_list.dart';
 import 'package:flutter/material.dart';
 import 'package:contacts_service/contacts_service.dart';
 
@@ -12,36 +13,34 @@ class _ContactPhonePageState extends State<ContactPhonePage> {
   final searchController = TextEditingController();
   bool isLoading = true;
   String search = "";
-  List<Contact> _contacts = [];
+  final List<Map<String, dynamic>> _contacts = [];
   List<String> names = [];
   List<String> phones = [];
-  List<Map<String, dynamic>> listContact = [];
+  List<Map<String, dynamic>> filteredList = [];
 
   Future<void> _fetchContacts() async {
     Iterable<Contact> contacts = await ContactsService.getContacts();
     setState(() {
-      _contacts = contacts.toList();
+      for (int i = 0; i < contacts.toList().length; i++) {
+        _contacts.add(
+          {
+            "name": contacts.toList()[i].displayName,
+          },
+        );
+        final phones = contacts.toList()[i].phones!;
+        for (var phone in phones) {
+          _contacts[i]["phone"] = phone.value;
+        }
+      }
       isLoading = false;
     });
-
-    for (int i = 0; i < contacts.toList().length; i++) {
-      listContact.add(
-        {
-          "name": contacts.toList()[i].displayName,
-        },
-      );
-      final phones = contacts.toList()[i].phones!;
-      for (var phone in phones) {
-        listContact[i]["phone"] = phone.value;
-      }
-    }
-    print(listContact);
   }
 
   @override
   void initState() {
     super.initState();
     _fetchContacts();
+    filteredList = _contacts;
   }
 
   @override
@@ -109,6 +108,7 @@ class _ContactPhonePageState extends State<ContactPhonePage> {
                                 searchController.text = "";
                                 setState(() {
                                   search = "";
+                                  filteredList = _contacts;
                                 });
                               },
                               icon: const Icon(Icons.close),
@@ -117,6 +117,7 @@ class _ContactPhonePageState extends State<ContactPhonePage> {
                     onChanged: (value) {
                       setState(() {
                         search = value;
+                        filteredList = searchItems(value, _contacts, false);
                       });
                     },
                   ),
@@ -129,41 +130,38 @@ class _ContactPhonePageState extends State<ContactPhonePage> {
                           color: Theme.of(context).primaryColor,
                         );
                       },
-                      itemCount: _contacts.length,
+                      itemCount: filteredList.length,
                       itemBuilder: (_, index) {
-                        var contact = _contacts[index];
+                        var contact = filteredList[index];
                         return ListTile(
                           onLongPress: () {
                             setState(() {
-                              names.contains(contact.displayName)
-                                  ? names.remove(contact.displayName!)
-                                  : names.add(contact.displayName!);
+                              names.contains(contact["name"])
+                                  ? names.remove(contact["name"])
+                                  : names.add(contact["name"]);
 
-                              phones.contains(contact.phones![0].value)
-                                  ? phones.remove(contact.phones![0].value)
-                                  : phones
-                                      .add(contact.phones![0].value.toString());
+                              phones.contains(contact["phone"])
+                                  ? phones.remove(contact["phone"])
+                                  : phones.add(contact["phone"]);
                             });
                           },
-                          selected: names.contains(contact.displayName),
+                          selected: names.contains(contact["name"]),
                           selectedTileColor: Colors.indigo,
                           selectedColor: Colors.white,
-                          title: Text(contact.displayName ?? ""),
+                          title: Text(contact["name"] ?? ""),
                           subtitle: Text(
-                            contact.phones!
-                                .map((phone) => phone.value)
-                                .join('\n'),
+                            contact["phone"],
                           ),
                           leading: CircleAvatar(
                             maxRadius: 25,
-                            backgroundColor: names.contains(contact.displayName)
+                            backgroundColor: names.contains(contact["name"])
                                 ? Colors.white
                                 : Theme.of(context).primaryColor,
-                            foregroundColor: names.contains(contact.displayName)
+                            foregroundColor: names.contains(contact["name"])
                                 ? Theme.of(context).primaryColor
                                 : Colors.white,
                             child: Text(
-                              contact.displayName.toString().split("")[0],
+                              contact["name"].toString().split("")[0],
                               style: const TextStyle(
                                 fontWeight: FontWeight.w700,
                               ),
