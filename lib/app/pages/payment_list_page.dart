bfb058ce408f0ddb6b7d70d8c05b8e9c2f473a99
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-09-03 11:16:13 -0300

    implementado o filtro por cliente

--- lib/app/pages/payment_list_page.dart
+++ lib/app/pages/payment_list_page.dart
@@ -1,6 +1,5 @@
 import 'package:app_kaike_barbearia/app/template/dialog_filter.dart';
 import 'package:app_kaike_barbearia/app/template/list_payment.dart';
-import 'package:app_kaike_barbearia/app/utils/search_list.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,120 +12,125 @@ class PaymentListPage extends StatefulWidget {
 
 class _PaymentListPageState extends State<PaymentListPage>
     with TickerProviderStateMixin {
+  final searchController = TextEditingController();
   late TabController _tabController;
   String optionSelected = "Tudo",
       lastOptionSale = "Tudo",
       lastOptionService = "Tudo",
-      tabSelected = "vendas";
-  List<Map<String, dynamic>> filteredPaymentsSales = [];
-  List<Map<String, dynamic>> filteredPaymentsServices = [];
-  List<Map<String, dynamic>> paymentsSales = [
-    {
-      "date_sale": "30/08/2023",
-      "value": 100.00,
-      "amount_paid": 100.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "client_name": "Valdirene Aparecida Ferreira",
-      "value": 200.00,
-      "amount_paid": 200.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "value": 100.00,
-      "amount_paid": 100.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "client_name": "Valdirene Aparecida Ferreira",
-      "value": 200.00,
-      "amount_paid": 200.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "value": 100.00,
-      "amount_paid": 100.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "client_name": "Valdirene Aparecida Ferreira",
-      "value": 200.00,
-      "amount_paid": 200.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "value": 100.00,
-      "amount_paid": 100.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "client_name": "Valdirene Aparecida Ferreira",
-      "value": 200.00,
-      "amount_paid": 200.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "value": 100.00,
-      "amount_paid": 0.0,
-      "situation": "A receber"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "client_name": "Valdirene Aparecida Ferreira",
-      "value": 200.00,
-      "amount_paid": 0.0,
-      "situation": "A receber"
-    },
-  ];
-  List<Map<String, dynamic>> paymentsServices = [
-    {
-      "date_sale": "30/08/2023",
-      "value": 100.00,
-      "amount_paid": 100.00,
-      "situation": "Recebido",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "client_name": "Joelmir Carvalho",
-      "value": 200.00,
-      "amount_paid": 200.00,
-      "situation": "Recebido",
-      "specie": "PIX"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "value": 100.00,
-      "amount_paid": 0.0,
-      "situation": "A receber",
-      "specie": "Dinheiro"
-    },
-    {
-      "date_sale": "30/08/2023",
-      "client_name": "Joelmir Carvalho",
-      "value": 200.00,
-      "amount_paid": 0.0,
-      "situation": "A receber",
-      "specie": "PIX"
-    },
-  ];
+      tabSelected = "vendas",
+      search = "";
+  bool searchByName = false;
+  final FocusNode _focusNode = FocusNode();
+  List<Map<String, dynamic>> filteredPaymentsSales = [],
+      filteredPaymentsByClient = [],
+      filteredPaymentsServices = [],
+      paymentsSales = [
+        {
+          "date_sale": "30/08/2023",
+          "value": 100.00,
+          "amount_paid": 100.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "client_name": "Valdirene Aparecida Ferreira",
+          "value": 200.00,
+          "amount_paid": 200.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "value": 100.00,
+          "amount_paid": 100.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "client_name": "Valdirene Aparecida Ferreira",
+          "value": 200.00,
+          "amount_paid": 200.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "value": 100.00,
+          "amount_paid": 100.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "client_name": "Valdirene Aparecida Ferreira",
+          "value": 200.00,
+          "amount_paid": 200.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "value": 100.00,
+          "amount_paid": 100.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "client_name": "Valdirene Aparecida Ferreira",
+          "value": 200.00,
+          "amount_paid": 200.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "value": 100.00,
+          "amount_paid": 0.0,
+          "situation": "A receber"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "client_name": "Valdirene Aparecida Ferreira",
+          "value": 200.00,
+          "amount_paid": 0.0,
+          "situation": "A receber"
+        },
+      ],
+      paymentsServices = [
+        {
+          "date_sale": "30/08/2023",
+          "value": 100.00,
+          "amount_paid": 100.00,
+          "situation": "Recebido",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "client_name": "Joelmir Carvalho",
+          "value": 200.00,
+          "amount_paid": 200.00,
+          "situation": "Recebido",
+          "specie": "PIX"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "value": 100.00,
+          "amount_paid": 0.0,
+          "situation": "A receber",
+          "specie": "Dinheiro"
+        },
+        {
+          "date_sale": "30/08/2023",
+          "client_name": "Joelmir Carvalho",
+          "value": 200.00,
+          "amount_paid": 0.0,
+          "situation": "A receber",
+          "specie": "PIX"
+        },
+      ];
 
   @override
   void initState() {
@@ -150,6 +154,13 @@ class _PaymentListPageState extends State<PaymentListPage>
               .where((paymentSale) =>
                   paymentSale["situation"].toString().contains(option))
               .toList();
+
+          if (search.isNotEmpty) {
+            filteredPaymentsSales = paymentsSales
+                .where((paymentSale) =>
+                    paymentSale["situation"].toString().contains(search))
+                .toList();
+          }
         } else {
           filteredPaymentsServices = paymentsServices
               .where((paymentService) =>
@@ -157,6 +168,63 @@ class _PaymentListPageState extends State<PaymentListPage>
               .toList();
         }
       }
+
+      clearTextFormField();
+    });
+  }
+
+  openDialogFilter() async {
+    optionSelected =
+        tabSelected == "vendas" ? lastOptionSale : lastOptionService;
+    final option = await showFilterDialog(context, optionSelected);
+    if (option == null) return;
+    filterLists(option);
+
+    setState(() {
+      optionSelected = option;
+      if (tabSelected == "vendas") {
+        lastOptionSale = option;
+      } else {
+        lastOptionService = option;
+      }
+    });
+  }
+
+  filterByClient() {
+    if (search.isNotEmpty) {
+      filteredPaymentsByClient = tabSelected == "vendas"
+          ? filteredPaymentsSales
+              .where((paymentSale) => paymentSale["client_name"]
+                  .toString()
+                  .toLowerCase()
+                  .contains(search.trim().toLowerCase()))
+              .toList()
+          : filteredPaymentsServices
+              .where((paymentService) => paymentService["client_name"]
+                  .toString()
+                  .toLowerCase()
+                  .contains(search.trim().toLowerCase()))
+              .toList();
+    } else {
+      filterLists(tabSelected == "vendas" ? lastOptionSale : lastOptionService);
+    }
+  }
+
+  toggleSearch() {
+    setState(() {
+      searchByName = !searchByName;
+    });
+
+    if (!searchByName) {
+      clearTextFormField();
+    }
+  }
+
+  clearTextFormField() {
+    searchController.text = "";
+    filterByClient();
+    setState(() {
+      search = "";
     });
   }
 
@@ -166,110 +234,155 @@ class _PaymentListPageState extends State<PaymentListPage>
       appBar: AppBar(
         title: const Text("Pagamentos"),
       ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.indigo.withOpacity(.1),
-            alignment: Alignment.centerRight,
-            child: IconButton(
-              onPressed: () async {
-                optionSelected = tabSelected == "vendas"
-                    ? lastOptionSale
-                    : lastOptionService;
-                final option = await showFilterDialog(context, optionSelected);
-                if (option == null) return;
-                filterLists(option);
-                setState(() {
-                  optionSelected = option;
-                  if (tabSelected == "vendas") {
-                    lastOptionSale = option;
-                  } else {
-                    lastOptionService = option;
-                  }
-                });
-              },
-              icon: const Icon(Icons.filter_list),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  width: 1,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
               color: Colors.indigo.withOpacity(.1),
-            ),
-            child: TabBar(
-              onTap: (value) {
-                if (value == 0) {
-                  tabSelected = "vendas";
-                } else {
-                  tabSelected = "serviços";
-                }
-              },
-              indicatorColor: Colors.indigo,
-              tabs: <Tab>[
-                Tab(
-                  text: null,
-                  icon: null,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.shopping_cart_rounded,
-                        color: Theme.of(context).primaryColor,
-                        size: 18,
-                      ),
-                      const SizedBox(width: 2),
-                      Text(
-                        "Vendas",
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 17,
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              alignment: Alignment.centerRight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    onPressed: () => toggleSearch(),
+                    icon: Icon(
+                      searchByName ? Icons.search_off_sharp : Icons.search,
+                      size: 30,
+                    ),
+                  ),
+                  Visibility(
+                    visible: searchByName,
+                    child: Expanded(
+                      child: TextFormField(
+                        focusNode: _focusNode,
+                        controller: searchController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          hintText: "Digite para buscar o cliente",
+                          suffixIcon: search.isEmpty
+                              ? null
+                              : IconButton(
+                                  onPressed: () {
+                                    searchController.text = "";
+                                    setState(() {
+                                      search = "";
+                                      filterLists(tabSelected == "vendas"
+                                          ? lastOptionSale
+                                          : lastOptionService);
+                                    });
+                                  },
+                                  icon: const Icon(Icons.close),
+                                ),
                         ),
+                        onChanged: (value) {
+                          setState(() {
+                            search = value;
+                            filterByClient();
+                          });
+                        },
                       ),
-                    ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () => openDialogFilter(),
+                    icon: const Icon(
+                      Icons.filter_list,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    width: 1,
+                    color: Theme.of(context).primaryColor,
                   ),
                 ),
-                Tab(
-                  text: null,
-                  icon: null,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        FontAwesomeIcons.screwdriverWrench,
-                        color: Theme.of(context).primaryColor,
-                        size: 18,
-                      ),
-                      const SizedBox(width: 2),
-                      Text(
-                        "Serviços",
-                        style: TextStyle(
+                color: Colors.indigo.withOpacity(.1),
+              ),
+              child: TabBar(
+                onTap: (value) {
+                  if (value == 0) {
+                    tabSelected = "vendas";
+                  } else {
+                    tabSelected = "serviços";
+                  }
+                },
+                indicatorColor: Colors.indigo,
+                tabs: <Tab>[
+                  Tab(
+                    text: null,
+                    icon: null,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.shopping_cart_rounded,
                           color: Theme.of(context).primaryColor,
-                          fontSize: 17,
+                          size: 18,
                         ),
-                      ),
-                    ],
+                        const SizedBox(width: 2),
+                        Text(
+                          "Vendas",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-              controller: _tabController,
+                  Tab(
+                    text: null,
+                    icon: null,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.screwdriverWrench,
+                          color: Theme.of(context).primaryColor,
+                          size: 18,
+                        ),
+                        const SizedBox(width: 2),
+                        Text(
+                          "Serviços",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+                controller: _tabController,
+              ),
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height - 240,
-            child: TabBarView(
-              controller: _tabController,
-              children: <Widget>[
-                ListPayment(payments: filteredPaymentsSales),
-                ListPayment(payments: filteredPaymentsServices)
-              ],
+            SizedBox(
+              height: MediaQuery.of(context).size.height - 260,
+              child: TabBarView(
+                controller: _tabController,
+                children: <Widget>[
+                  ListPayment(
+                      payments: search.isNotEmpty
+                          ? filteredPaymentsByClient
+                          : filteredPaymentsSales),
+                  ListPayment(
+                      payments: search.isNotEmpty
+                          ? filteredPaymentsByClient
+                          : filteredPaymentsServices)
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
