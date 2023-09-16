533c33e98080cfcbd8da82b95717866e0568e120
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-09-16 15:09:52 -0300

    payment_service_model

--- lib/app/models/payment_service_model.dart
+++ lib/app/models/payment_service_model.dart
@@ -1,3 +1,4 @@
+import 'package:app_kayke_barbearia/app/config/db.dart';
 import 'package:sqflite/sqflite.dart';
 
 class PaymentService {
@@ -16,11 +17,55 @@ class PaymentService {
   });
 
   Future<void> save(Transaction txn) async {
-    await txn.insert("payments_services", {
-      "specie": specie,
-      "amount_paid": amountPaid,
-      "date_payment": datePayment,
-      "provision_of_service_id": provisionOfServiceId
-    },);
+    await txn.insert(
+      "payments_services",
+      {
+        "specie": specie,
+        "amount_paid": amountPaid,
+        "date_payment": datePayment,
+        "provision_of_service_id": provisionOfServiceId
+      },
+    );
+  }
+
+  Future<int> reserve() async {
+    int lastId = 0;
+    final db = await DB.openDatabase();
+    if (id == 0) {
+      lastId = await db.insert(
+        "payments_services",
+        {
+          "specie": specie,
+          "amount_paid": amountPaid,
+          "date_payment": datePayment,
+          "provision_of_service_id": provisionOfServiceId
+        },
+      );
+    } else {
+      await db.update(
+        "payments_services",
+        {
+          "specie": specie,
+          "amount_paid": amountPaid,
+          "date_payment": datePayment,
+        },
+        where: "id = ?",
+        whereArgs: [id],
+      );
+    }
+
+    return lastId;
+  }
+
+  static Future<List<Map<String, dynamic>>> findBySaleId(int provisionOfServiceId) async {
+    final db = await DB.openDatabase();
+    return db.rawQuery(
+        "SELECT id, amount_paid AS value, date_payment AS date, specie, provision_of_service_id  FROM payments_services WHERE provision_of_service_id = ?",
+        [provisionOfServiceId]);
+  }
+
+  static void delete(int id) async {
+    final db = await DB.openDatabase();
+    await db.delete("payments_services", where: "id = ?", whereArgs: [id]);
   }
 }
--- lib/app/models/provision_of_services_model.dart
+++ lib/app/models/provision_of_services_model.dart
@@ -63,8 +63,8 @@ class ProvisionOfService {
   static Future<List<Map<String, dynamic>>> findByDate(String date) async {
     final db = await DB.openDatabase();
     return db.rawQuery("SELECT provision_of_services.id, provision_of_services.value_total, "
-        "(SELECT SUM(amount_paid) FROM payments_services WHERE id = provision_of_services.id) AS amount_paid, "
-        "CASE WHEN (provision_of_services.value_total - (SELECT SUM(amount_paid) FROM payments_services WHERE id = provision_of_services.id) = 0) "
+        "(SELECT SUM(amount_paid) FROM payments_services WHERE provision_of_service_id = provision_of_services.id) AS amount_paid, "
+        "CASE WHEN (provision_of_services.value_total - (SELECT SUM(amount_paid) FROM payments_services WHERE provision_of_service_id = provision_of_services.id) = 0) "
         "THEN 'Recebido' "
         "ELSE 'A receber' "
         "END AS situation, "
